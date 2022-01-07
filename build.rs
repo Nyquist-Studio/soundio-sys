cfa98dfbc3d9c101628aa7196a6180bf18896cb4
Yan Li <eyeplum@gmail.com>
2022-01-07 18:01:29 +1300

    Link to macOS system frameworks

--- build.rs
+++ build.rs
@@ -10,33 +10,46 @@ fn main() {
 
 fn build_and_link_soundio() -> PathBuf {
     let soundio_build_output = cmake::Config::new("./libsoundio/")
-        .define("BUILD_STATIC_LIBS", "true")
-        .define("BUILD_DYNAMIC_LIBS", "false")
-        .define("BUILD_EXAMPLE_PROGRAMS", "false")
-        .define("BUILD_TESTS", "false")
-        .define("ENABLE_JACK", "true")
-        .define("ENABLE_PULSEAUDIO", "true")
-        .define("ENABLE_ALSA", "true")
-        .define("ENABLE_COREAUDIO", "true")
-        .define("ENABLE_WASAPI", "true")
+        .define("BUILD_STATIC_LIBS", "ON")
+        .define("BUILD_DYNAMIC_LIBS", "OFF")
+        .define("BUILD_EXAMPLE_PROGRAMS", "OFF")
+        .define("BUILD_TESTS", "OFF")
+        .define("ENABLE_JACK", "ON")
+        .define("ENABLE_PULSEAUDIO", "ON")
+        .define("ENABLE_ALSA", "ON")
+        .define("ENABLE_COREAUDIO", "ON")
+        .define("ENABLE_WASAPI", "ON")
         .build();
 
     println!(
         "cargo:rustc-link-search=native={}",
-        format!("{}/lib", soundio_build_output.display())
+        soundio_build_output.join("lib").display()
     );
     println!("cargo:rustc-link-lib=static=soundio");
+
+    #[cfg(target_os = "macos")]
+    link_macos_system_frameworks();
 
     soundio_build_output
 }
 
-fn generate_rust_binding(build_output: &Path) {
-    let umbrella_header_path = format!("{}/include/soundio/soundio.h", build_output.display());
+#[cfg(target_os = "macos")]
+fn link_macos_system_frameworks() {
+    println!("cargo:rustc-link-lib=framework=AudioToolbox");
+    println!("cargo:rustc-link-lib=framework=CoreAudio");
+    println!("cargo:rustc-link-lib=framework=CoreFoundation");
+}
 
-    println!("cargo:rerun-if-changed={}", umbrella_header_path);
+fn generate_rust_binding(build_output: &Path) {
+    let umbrella_header_path = build_output
+        .join("include")
+        .join("soundio")
+        .join("soundio.h");
+
+    println!("cargo:rerun-if-changed={}", umbrella_header_path.display());
 
     let bindings = bindgen::Builder::default()
-        .header(umbrella_header_path)
+        .header(umbrella_header_path.to_str().unwrap())
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Failed to generate bindings");
